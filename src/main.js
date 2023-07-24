669c65800337fbd12a8da9e35a44277e1d7edf8b
Lukas Böhler <lukas@boehlerbrothers.com>
2023-07-24 14:22:34 +0200

    Init.

---
+++ src/main.js
@@ -0,0 +1,8 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import Gleap from 'gleap';
+
+// Please make sure to call this method only once!
+Gleap.initialize("ogWhNhuiZcGWrva5nlDS8l7a78OfaLlV");
+
+createApp(App).mount('#app')
